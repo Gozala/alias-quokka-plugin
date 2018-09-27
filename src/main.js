b823376ef10d2dfd3471f05423c727d25271c54c
Dave Stewart <info@davestewart.co.uk>
2018-09-27 15:36:05 +0100

    Add support for partial paths

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
-"use strict"
+'use strict'
 
-const Path = require("path")
-const { _load, Module } = require("module")
+const Path = require('path')
+const { _load, Module } = require('module')
+
+function resolvePath (aliases, src) {
+  let key, path
+  for (key in aliases) {
+    path = aliases[key]
+    if (src.indexOf(key) === 0) {
+      return src.replace(key, path)
+    }
+  }
+  return null
+}
 
 exports.before = config => {
-  const root = Path.resolve(".")
+  const root = Path.resolve('.')
+  const aliases = config.alias || {}
   Module._load = (path, ...rest) => {
-    const alias = config.alias && config.alias[path]
-    if (alias) {
-      return _load(Path.join(root, alias), ...rest)
-    } else {
-      return _load(path, ...rest)
-    }
+    const src = resolvePath(aliases, path)
+    return src
+      ? _load(Path.join(root, src), ...rest)
+      : _load(path, ...rest)
   }
 }
