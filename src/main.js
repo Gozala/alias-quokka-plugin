3a9cfdf5d431ffb250408547283a1a4f6e8b0c47
Dave Stewart <info@davestewart.co.uk>
2018-09-27 17:45:03 +0100

    Extract all logic to functions

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const Path = require('path')
 const { _load, _findPath, Module } = require('module')
 
-function resolvePath (aliases, src) {
+function resolveAlias (aliases, src) {
   let key, path
   for (key in aliases) {
     path = aliases[key]
@@ -15,21 +15,22 @@ function resolvePath (aliases, src) {
   return null
 }
 
-exports.before = config => {
+function resolvePath (src, path) {
   const root = Path.resolve('.')
+  const relPath = Path.join(root, src)
+  const absPath = _findPath(relPath)
+  if (fs.existsSync(absPath)) {
+    return absPath
+  }
+  throw new Error('Unable to resolve module "' +src+ '" via aliased path "' + path + '"')
+}
+
+exports.before = config => {
   const aliases = config.alias || {}
   Module._load = (path, ...rest) => {
-    const src = resolvePath(aliases, path)
-    if (src) {
-      const relPath = Path.join(root, src)
-      const absPath = _findPath(relPath)
-      if (fs.existsSync(absPath)) {
-        return _load(relPath, ...rest)
-      }
-      else {
-        throw new Error('Unable to resolve module "' +src+ '" via aliased path "' + path + '"')
-      }
-    }
-    return _load(path, ...rest)
+    const src = resolveAlias(aliases, path)
+    return src
+      ? _load(resolvePath(src, path), ...rest)
+      : _load(path, ...rest)
   }
 }
