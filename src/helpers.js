87407807bd47975a5f5f267bc7cec61bb54e9465
Dave Stewart <dave@davestewart.co.uk>
2018-09-30 20:06:23 +0100

    Add support for external alias configurations

---
+++ src/helpers.js
@@ -0,0 +1,109 @@
+const get = require("object-get")
+const Path = require("path")
+const fs = require("fs")
+const { _findPath } = require("module")
+const { message, clean } = require("./utils")
+
+/**
+ * Build optimised alias tree
+ *
+ * - keys sorted and reversed
+ * - paths stored as arrays
+ *
+ * @param   {object}  input   An object of alias:path or alias:[path, path, path] pairs
+ * @returns an object of arrays
+ */
+function optimiseAliases(input) {
+  const output = {}
+
+  Object.keys(input)
+    .sort()
+    .reverse()
+    .forEach(key => {
+      const data = input[key]
+      output[clean(key)] = Array.isArray(data)
+        ? data.map(path => clean(path))
+        : [clean(data)]
+    })
+
+  return output
+}
+
+/**
+ * Load aliases from user-supplied file
+ *
+ * @param   {string}  file  The relative path to a configuration file
+ * @param   {string}  node  The child node path to parse for aliases
+ */
+function loadAliases(file, node) {
+  const path = Path.join(Path.resolve("."), file)
+
+  const config = require(path)
+  if (typeof config !== "object") {
+    throw new Error(message(`no config file found at path '${path}'`))
+  }
+
+  const data = get(config, node || "")
+  if (!data) {
+    throw new Error(
+      message(`no alias data found in file '${file}' at node '${node}'`)
+    )
+  }
+
+  return optimiseAliases(data)
+}
+
+/**
+ * Get path(s) for a given alias
+ *
+ * - full alias must match start of path
+ *
+ * @param   {object}  aliases   A hash of aliases
+ * @param   {string}  alias     The alias to find
+ * @returns {string[]|undefined}
+ */
+function getAlias(aliases, alias) {
+  const key = Object.keys(aliases).find(key => alias.indexOf(key) === 0)
+
+  if (key) {
+    return { id: key, paths: aliases[key] }
+  }
+}
+
+/**
+ * Resolve an absolute path for a target based on alias and
+ *
+ * @param   {string}  target  The target module path including the alias, i.e. @/classes/SomeClass
+ * @param   {string}  alias   The alias to replace, i.e @/classes/
+ * @param   {array}   paths   An array of paths where the resolved target module might be found
+ * @returns {string}
+ */
+function resolvePath(target, alias, paths) {
+  const root = Path.resolve(".")
+
+  for (let i = 0; i < paths.length; i++) {
+    const relPath = target.replace(alias, paths[i])
+    const modPath = !relPath.startsWith("/")
+      ? Path.join(root, relPath)
+      : relPath
+    const absPath = _findPath(modPath)
+
+    if (fs.existsSync(absPath)) {
+      return absPath
+    }
+  }
+
+  throw new Error(
+    'Cannot resolve aliased module "' +
+      target +
+      '" on alias path(s) ' +
+      paths.join(", ")
+  )
+}
+
+module.exports = {
+  loadAliases,
+  optimiseAliases,
+  getAlias,
+  resolvePath
+}
--- src/main.js
+++ src/main.js
@@ -1,36 +1,29 @@
-'use strict'
+"use strict"
 
-const fs = require('fs')
-const Path = require('path')
-const { _load, _findPath, Module } = require('module')
-
-function resolveAlias (aliases, src) {
-  const key = Object
-    .keys(aliases)
-    .sort()
-    .reverse()
-    .find(key => src.indexOf(key) === 0)
-  return key
-    ? src.replace(key, aliases[key])
-    : undefined
-}
-
-function resolvePath (src, path) {
-  const root = Path.resolve('.')
-  const relPath = Path.join(root, src)
-  const absPath = _findPath(relPath)
-  if (fs.existsSync(absPath)) {
-    return absPath
-  }
-  throw new Error('Cannot resolve module "' +src+ '" via aliased path "' + path + '"')
-}
+const { _load, Module } = require("module")
+const {
+  loadAliases,
+  optimiseAliases,
+  getAlias,
+  resolvePath
+} = require("./helpers")
+const { inspect } = require("./utils")
 
 exports.before = config => {
-  const aliases = config.alias || {}
-  Module._load = (path, ...rest) => {
-    const src = resolveAlias(aliases, path)
-    return src
-      ? _load(resolvePath(src, path), ...rest)
-      : _load(path, ...rest)
+  const settings = config.alias || {}
+
+  let aliases = settings.import
+    ? loadAliases(settings.import.file, settings.import.node)
+    : optimiseAliases(settings.aliases || {})
+
+  if (settings.debug) {
+    inspect("aliases", aliases)
+  }
+
+  Module._load = function(target, ...rest) {
+    const alias = getAlias(aliases, target)
+    return alias
+      ? _load(resolvePath(target, alias.id, alias.paths), ...rest)
+      : _load(target, ...rest)
   }
 }
